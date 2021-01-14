8a7be099f3e3d6f5ce4b165bbf601f4a3fbf0e5d
pedro guimaraes <pedrof.guima197@gmail.com>
2021-01-13 22:29:41 -0300

    desafio reactjs gostack 2020

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then((r) => setProjects(r.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    await api
+      .post("/repositories",{
+        title: `Repositorio ${Date.now()}`,
+      })
+      .then((r) => setProjects([...projects, r.data]));
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api
+      .delete(`/repositories/${id}`)
+      .then((r) => setProjects(projects.filter((p) => p.id !== id)));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {projects && projects.map((proj) => (
+          <li key={proj.id}>
+            {proj.title}
+            <button onClick={() => handleRemoveRepository(proj.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
